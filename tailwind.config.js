eb5416a8f39efa43bdb277659fca39b2aa24728c
Zeke Abshire <zekeabshire@gmail.com>
2022-12-17 16:57:40 -0600

    setup configs

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        xs: "400px",
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+      },
+    },
+    extend: {
+      colors: {
+        bg: "#151515",
+        fg: "#ffffff",
+        "gruv-red": "#EA6962",
+        "gruv-yel": "#E78A4E",
+        "gruv-grn": "#A9B665",
+        "gruv-cya": "#89B482",
+        "gruv-blu": "#7DAEA3",
+        "gruv-mag": "#D3869B",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
