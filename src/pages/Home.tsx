f3b9621602efca1331a0db918b20e6881f8055c5
Zeke Abshire <zekeabshire@gmail.com>
2023-01-01 18:43:29 -0600

    adjusted styles + addressed refactors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,40 @@
-import { MdEmail } from "react-icons/md";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { RxDoubleArrowDown } from "react-icons/rx";
+import { GithubIcon, EmailIcon, LinkedinIcon } from "components/Icons";
 
 export default function Home() {
   return (
     <>
-      <h1 className="mt-[20vh] text-3xl font-semibold leading-normal xs:text-4xl xs:leading-normal sm:text-6xl sm:leading-snug md:text-7xl md:leading-snug lg:text-8xl lg:leading-tight">
-        Full-Stack
-        <br />
-        Software Developer
-      </h1>
-
-      <div className="mt-2 flex flex-row justify-between text-xl font-light text-mix sm:text-2xl md:mt-5 md:text-3xl lg:text-3xl">
-        <h2>Zeke Abshire</h2>
-        <div className="flex flex-row items-center gap-x-4">
-          <a href="https://github.com/zyrrus/">
-            <FaGithub className="text-mix hover:text-fg" />
-          </a>
-          <a href="https://www.linkedin.com/in/zyrrus/">
-            <FaLinkedin className="text-mix hover:text-fg" />
-          </a>
-          <a href="mailto:zekeabshire@gmail.com">
-            <MdEmail className="text-mix hover:text-fg" />
-          </a>
+      <div className="flex min-h-screen flex-col items-center justify-between gap-y-12">
+        <div className="w-full">
+          <h1 className="mt-36 text-2xl font-semibold leading-normal xs:text-3xl xs:leading-normal sm:text-5xl sm:leading-snug md:text-6xl md:leading-snug lg:text-7xl lg:leading-tight">
+            Full-Stack
+            <br />
+            Software Developer
+          </h1>
+          <div className="mt-2 flex flex-row justify-between text-xl font-light text-mix sm:text-2xl md:mt-5 md:text-3xl lg:text-3xl">
+            <h2>Zeke Abshire</h2>
+            <div className="flex flex-row items-center gap-x-4">
+              <a href="https://github.com/zyrrus/">
+                <GithubIcon />
+              </a>
+              <a href="https://www.linkedin.com/in/zyrrus/">
+                <LinkedinIcon />
+              </a>
+              <a href="mailto:zekeabshire@gmail.com">
+                <EmailIcon />
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <a
-        href="#intro"
-        className="absolute top-[75vh] left-1/2 mx-auto block grow -translate-x-1/2 transform"
-      >
-        <RxDoubleArrowDown className="floating text-6xl text-mix hover:text-fg sm:text-6xl md:text-7xl lg:text-8xl" />
-      </a>
+        <a href="#intro" className="mb-48">
+          <RxDoubleArrowDown className="floating text-6xl text-mix transition-colors hover:text-fg sm:text-6xl md:text-7xl lg:text-8xl" />
+        </a>
+      </div>
 
       <div
         id="intro"
-        className="container absolute left-1/2 top-[100vh] flex h-screen translate-x-[-50%] flex-col gap-y-10 py-8 text-lg leading-loose text-mix"
+        className="flex flex-col gap-y-10 py-8 text-lg leading-loose text-mix"
       >
         <p>
           Hi, my name is Zeke Abshire, and I am currently a Software Developer
@@ -56,6 +55,9 @@ export default function Home() {
           please don't hesitate to contact me.
         </p>
       </div>
+
+      {/* Spacer for Footer */}
+      {/* <div className="h-screen" /> */}
     </>
   );
 }
