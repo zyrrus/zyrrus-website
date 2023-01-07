ff98205b1bc85513b55e15721cccee536b08d1fd
Zeke Abshire <zekeabshire@gmail.com>
2023-01-06 18:24:08 -0600

    resolved the last key error

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,6 +1,7 @@
 import { Project } from "data/projects";
-import SmallDivider from "components/Divider";
+import { SmallDivider } from "components/Divider";
 import ProjectItem from "components/ProjectItem";
+import { Fragment } from "react";
 
 type Props = {
   title: string;
@@ -12,12 +13,10 @@ export default function ProjectList({ title, projects }: Props) {
     <>
       <h1 className="my-6 mt-14 text-4xl font-semibold">{title}</h1>
       {projects.map((project, index) => (
-        <>
-          <ProjectItem key={`Project-${project.title}`} {...project} />
-          {index !== projects.length - 1 && (
-            <SmallDivider key={`Divider-${project.title}`} />
-          )}
-        </>
+        <Fragment key={project.title}>
+          <ProjectItem {...project} />
+          {index !== projects.length - 1 && <SmallDivider />}
+        </Fragment>
       ))}
     </>
   );
