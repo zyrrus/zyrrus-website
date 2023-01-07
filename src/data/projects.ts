93fe5a8dc5f31ac1439c2f3aab10b22fb1682472
Zeke Abshire <zekeabshire@gmail.com>
2023-01-07 02:30:31 -0600

    added flurry figma link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -93,7 +93,10 @@ const ProjectsList: {
       title: "Flurry | Personal Project",
       stack: "TBD",
       date: "Dec 2022 - Present",
-      links: {},
+      links: {
+        figma:
+          "https://www.figma.com/file/jK6Sks2baD9CHhISlLowva/Flurry?node-id=1%3A2&t=Cy2r7kSEeoHuMAUq-1",
+      },
       description: [
         "I'm currently working on a personal project called Flurry, a language learning app that aims to combine the best features of the most popular apps while remaining free with an optional, unobtrusive paid plan. I spent much of the first month researching and testing a handful of the most popular language learning apps and teaching methods to gather ideas and inspiration for Flurry. I'm excited to begin developing this project further and to bring a unique and valuable language learning experience to users.",
       ],
