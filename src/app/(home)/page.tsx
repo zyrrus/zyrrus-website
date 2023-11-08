a6c7d7ad8f9caaf103bc60aefbad7b1f271882c5
Zeke Abshire <zekeabshire@gmail.com>
2023-11-07 21:48:07 -0600

    Temporary svg mask set up

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,13 @@ import Hero from "~/app/(home)/hero-section";
 import About from "~/app/(home)/about-section";
 import Featured from "~/app/(home)/featured-section";
 import Articles from "~/app/(home)/articles-section";
+import { HeroGraphic } from "~/app/(home)/hero-graphic";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-52 pb-24 pt-72">
       {/* <RingBackground /> */}
-      {/* <HeroGraphic /> */}
+      <HeroGraphic />
       <Hero />
       <About />
       <Featured />
