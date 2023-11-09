4b906e90c1731c181480cca987e505b5e13f7fb2
Zeke Abshire <zekeabshire@gmail.com>
2023-11-09 02:54:03 -0600

    Removed dot. Added copyright

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,25 +3,31 @@ import { Button } from "~/app/components/ui/button";
 const Footer = () => {
   return (
     <footer className="container mb-24 mt-10 text-center">
-      <Button variant="link" asChild>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/zyrrus"
-        >
-          github.com/zyrrus
-        </a>
-      </Button>
-      {" · "}
-      <Button variant="link" asChild>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://linkedin.com/in/zyrrus"
-        >
-          linkedin.com/in/zyrrus
-        </a>
-      </Button>
+      <div className="flex flex-col gap-y-3">
+        <div>
+          <Button variant="link" asChild>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/zyrrus"
+            >
+              github.com/zyrrus
+            </a>
+          </Button>
+          <Button variant="link" asChild>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://linkedin.com/in/zyrrus"
+            >
+              linkedin.com/in/zyrrus
+            </a>
+          </Button>
+        </div>
+        <p className="text-sm text-secondary">
+          &copy; {new Date().getFullYear()} Zeke Abshire
+        </p>
+      </div>
     </footer>
   );
 };
